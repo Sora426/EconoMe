6c8d63581a094d15f9e86780a1350f7600f99194
Ruhsora <250268@newuu.uz>
2026-08-06 19:46:39 +0500

    uffffffffffffffffffffffff

--- services/mailService.js
+++ services/mailService.js
@@ -1,5 +1,8 @@
 const nodemailer = require("nodemailer");
-
+console.log("SMTP_HOST =", process.env.SMTP_HOST);
+console.log("SMTP_PORT =", process.env.SMTP_PORT);
+console.log("SMTP_USER =", process.env.SMTP_USER);
+console.log("EMAIL_FROM =", process.env.EMAIL_FROM);
 const transporter = nodemailer.createTransport({
 
     host: process.env.SMTP_HOST,
