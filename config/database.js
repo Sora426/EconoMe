a7a9fd348535ffad30762fbb0f043cadefc3f3c4
Ruhsora <250268@newuu.uz>
2026-08-06 15:22:05 +0500

    heyyy

--- config/database.js
+++ config/database.js
@@ -1,15 +1,23 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
+const fs = require("fs");
 
-const db = new sqlite3.Database(
-    path.join(__dirname, "../econome.db"),
-    (err) => {
-        if (err) {
-            console.log(err.message);
-        } else {
-            console.log("Connected to SQLite");
-        }
+const diskDb = "/var/data/econome.db";
+const localDb = path.join(__dirname, "../econome.db");
+
+// First deployment after adding the disk:
+// copy the existing database to the disk if it isn't there yet.
+if (!fs.existsSync(diskDb) && fs.existsSync(localDb)) {
+    fs.copyFileSync(localDb, diskDb);
+    console.log("Database copied to persistent disk.");
+}
+
+const db = new sqlite3.Database(diskDb, (err) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        console.log("Connected to SQLite (Persistent Disk)");
     }
-);
+});
 
 module.exports = db;
