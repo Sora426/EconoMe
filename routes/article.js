d60f21ea2f2ecce21a374fca1b89b6299feeec34
Ruhsora <250268@newuu.uz>
2026-08-08 06:47:14 +0500

    o

--- routes/article.js
+++ routes/article.js
@@ -1,7 +1,7 @@
 const express=require("express");
 
 const router=express.Router();
-
+const upload = require("../config/multer");
 const articleController=require("../controllers/articleController");
 
 router.get("/",articleController.index);
